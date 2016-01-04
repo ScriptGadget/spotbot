3098d7de8e0cca8f2e9e348f9c52791329635b58
Bill Glover <bill@billglover.com>
2016-01-04 18:54:20 -0500

    keep all of the redis dependencies confined to subscription

--- spotbot/subscription.py
+++ spotbot/subscription.py
@@ -6,15 +6,30 @@ Retrieves alert subscriptions from redis
 
 import redis
 
-def get_subscriptions(redis_store):
+def get_subscriptions(state_store):
     """ Pulls all of the subscriptions from the provided Redis store"""
     alarms = {}
     cursor = 0
     while True:
-        cursor, keys = redis_store.scan(cursor, 'sb:alarm*')
+        cursor, keys = state_store.scan(cursor, 'sb:alarm*')
         for key in keys:
-            alarms[key] = redis_store.hgetall(key)
+            alarms[key] = state_store.hgetall(key)
             alarms[key]['redis_key'] = key
         if cursor == 0:
             break
     return alarms
+
+def get_state_store():
+    return redis.StrictRedis()
+
+def get_last_scan(state_store):
+    return state_store.get('sb:last_scan')
+
+def set_last_scan(state_store, last_scan):
+    state_store.set('sb:last_scan', last_scan)
+
+def get_web_hook(state_store, user):
+    return state_store.hget('sb:user#' + user, 'slack_webhook')
+
+def get_notification_channel(state_store, user):
+    return state_store.hget('sb:user#' + user, 'slack_channel')
