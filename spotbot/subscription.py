c4b1a11eaf287d5ca24abbd9149482012629126f
Bill Glover <bill@billglover.com>
2016-02-16 20:25:45 -0500

    remove redis and use shelve for now

--- spotbot/subscription.py
+++ spotbot/subscription.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python
 """
-Retrieves alert subscriptions from redis
+Manage the subscriptions using a shelf
 
 """
 
-import redis
+import shelve
 
 def get_subscriptions(state_store):
-    """ Pulls all of the subscriptions from the provided Redis store"""
-    alarms = {}
-    cursor = 0
-    while True:
-        cursor, keys = state_store.scan(cursor, 'sb:alarm*')
-        for key in keys:
-            alarms[key] = state_store.hgetall(key)
-            alarms[key]['redis_key'] = key
-        if cursor == 0:
-            break
-    return alarms
+    """ Pulls all of the subscriptions from the provided store"""
+    return state_store['subscriptions']
 
 def get_state_store():
-    return redis.StrictRedis()
+    return shelve.open('spotbot-shelf', writeback=True);
 
 def get_last_scan(state_store):
-    return state_store.get('sb:last_scan')
+    return state_store['last_scan'] if state_store.has_key('last_scan') else None 
 
 def set_last_scan(state_store, last_scan):
-    state_store.set('sb:last_scan', last_scan)
+    state_store['last_scan'] = last_scan
 
 def get_web_hook(state_store, user):
-    return state_store.hget('sb:user#' + user, 'slack_webhook')
+    return state_store['users'][user]['slack_webhook']
 
 def get_notification_channel(state_store, user):
-    return state_store.hget('sb:user#' + user, 'slack_channel')
+    return state_store['users'][user]['slack_channel']
 
-def update_subscription_with_last_result(redis_store, key, alert):
-    return redis_store.hset(key, 'last_alert', alert['last_alert'])
+def update_subscription_with_last_result(state_store, key, alert):
+    subscriptions = get_subscriptions(state_store)
+    subscription = subscriptions[key]
+    subscription['last_alert'] = alert['last_alert']
+    subscriptions['key'] = subscription
+    state_store['subscriptions'] = subscriptions
+
+def close_state_store(state_store):
+    state_store.sync()
+    state_store.close()
